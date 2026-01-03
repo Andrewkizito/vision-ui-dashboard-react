63d558df1ab9c5348742a093aa24b76b1c673511
andrewkizito <andrewkizito54@gmail.com>
2026-01-03 20:27:37 +0300

    Fix formatting and reintroduce VisionUIControllerProvider in index.js

--- src/index.js
+++ src/index.js
@@ -17,19 +17,20 @@
 */
 
 import React from "react";
-import { createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
 
 // Vision UI Dashboard React Context Provider
 import { VisionUIControllerProvider } from "context";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-
-root.render(<BrowserRouter>
-    <App />
-  </VisionUIControllerProvider>
-</BrowserRouter>)
-
+root.render(
+  <BrowserRouter>
+    <VisionUIControllerProvider>
+      <App />
+    </VisionUIControllerProvider>
+  </BrowserRouter>
+);

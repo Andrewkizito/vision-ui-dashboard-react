deb028801e16c264613c5aecdda3f4ee3ee7f33a
andrewkizito <andrewkizito54@gmail.com>
2026-01-06 07:23:52 +0300

    chore: reintroduce comment about React 18 in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "App";
 import { VisionUIControllerProvider } from "context";
 
+// Yes indeed, I love React 18
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 

d7337ee956f4c4b9bb8a7cef6df94734a59ad624
andrewkizito <andrewkizito54@gmail.com>
2026-01-06 07:12:32 +0300

    fix: test build

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
-
-// Vision UI Dashboard React Context Provider
 import { VisionUIControllerProvider } from "context";
 
 const rootElement = document.getElementById("root");

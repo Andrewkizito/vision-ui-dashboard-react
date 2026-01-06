9efc3d07c99d59cbf7a4ce372a335e2c5fbb8930
andrewkizito <andrewkizito54@gmail.com>
2026-01-06 07:26:56 +0300

    chore: remove outdated comment about React 18's new root API in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "App";
 import { VisionUIControllerProvider } from "context";
 
-// Yes indeed, I love React 18's new root API
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
